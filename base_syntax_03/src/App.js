f7398ba5d398353067229df8375cc9422b9eb9cb
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2018-12-19 11:41:56 -0600

    set class names dynamically

--- base_syntax_03/src/App.js
+++ base_syntax_03/src/App.js
@@ -67,8 +67,19 @@ deletePersonHandler = (personIndex) => {
         style.backgroundColor = 'red'
     }
 
+    //const classes = ['red', 'bold'].join(' ');
+    const classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
+        <h1>This is a react app!</h1>
+        <p className = {classes.join(' ')}> This is working! </p>
         <button
           style = {style}
           onClick={() => this.togglePersonsHandler()}>Toggle Persons
